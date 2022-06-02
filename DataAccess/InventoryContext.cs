fe2a7c3a881044310d164d39445e8f0c6035ab23
Didier Hobak <dual.didier.hobak@gmail.com>
2022-06-02 13:25:23 -0500

    21.- Alimentando la base de datos (Migracion condatos creada, Base de datos creada)

--- DataAccess/InventoryContext.cs
+++ DataAccess/InventoryContext.cs
@@ -23,5 +23,31 @@ namespace DataAccess
                 options.UseSqlServer(@"Data Source=DESKTOP-IGNH3JV\BOCCHI;Initial Catalog=Inventory;Integrated Security=True");    //StringConection
             }
         }
+
+        //Cargar datos antes de crear la BD
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+            base.OnModelCreating(modelBuilder);     //base: metodo que ya existe dentro de DBContext
+
+            modelBuilder.Entity<CategoryEntity>().HasData(  //Cateroria
+                new CategoryEntity { CategoryId = "VDG", CategoryName = "Video Games" },
+                new CategoryEntity { CategoryId = "STW", CategoryName = "Software" },
+                new CategoryEntity { CategoryId = "DVD", CategoryName = "Video" },
+                new CategoryEntity { CategoryId = "AUD", CategoryName = "Audio" },
+                new CategoryEntity { CategoryId = "GTS", CategoryName = "Gadgets" },
+                new CategoryEntity { CategoryId = "MSC", CategoryName = "Music" }
+                );
+
+            modelBuilder.Entity<WarehouseEntity>().HasData(  //bodega
+                new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega Central", WarehouseAddress = "Los santos" },
+                new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega local", WarehouseAddress = "Grove Street" },
+                new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega Norte", WarehouseAddress = "Las Venturas" }
+                );
+
+            //modelBuilder.Entity<ProductEntity>().HasData(  //producto
+            //    new ProductEntity { ProductId = "NR4-59264", ProductName = "RTX 4090 Ti", PuoductDescription = "a" },
+            //    new ProductEntity { ProductId = "IC9-59264", ProductName = "Intel I12-15900K", PuoductDescription = "a" }
+            //    );
+        }
     }
 }
---
+++ DataAccess/Migrations/20220602181820_AddData.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class AddData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[,]
+                {
+                    { "AUD", "Audio" },
+                    { "DVD", "Video" },
+                    { "GTS", "Gadgets" },
+                    { "MSC", "Music" },
+                    { "STW", "Software" },
+                    { "VDG", "Video Games" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Warehouses",
+                columns: new[] { "WarehouseId", "WarehouseAddress", "WarehouseName" },
+                values: new object[,]
+                {
+                    { "38f41e16-610a-48fd-97c0-8b69b4234ae8", "Los santos", "Bodega Central" },
+                    { "5f208590-6b93-4d51-ba23-1e1faa48fb89", "Grove Street", "Bodega local" },
+                    { "f41d1592-48b5-4ab5-ba68-1ecddd9d21ac", "Las Venturas", "Bodega Norte" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "AUD");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "DVD");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "GTS");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "MSC");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "STW");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "VDG");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: "38f41e16-610a-48fd-97c0-8b69b4234ae8");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: "5f208590-6b93-4d51-ba23-1e1faa48fb89");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: "f41d1592-48b5-4ab5-ba68-1ecddd9d21ac");
+        }
+    }
+}
