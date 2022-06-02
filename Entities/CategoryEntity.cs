533e547ce977919dfd62a395bbd35f411b4dcb5f
Didier Hobak <dual.didier.hobak@gmail.com>
2022-06-02 11:07:47 -0500

    16.- Creación de entidades (Categoria)

---
+++ Entities/CategoryEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class CategoryEntity
+    {
+        [Key]  //DataAnotations
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
